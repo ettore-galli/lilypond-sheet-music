5e38f9bf48b8438f80e57856a6ada3c45bb8e1bf
ettore-galli <ettore.galli@gmail.com>
2026-09-01 07:30:35 +0200

    Cleanup

--- drone-app/src/App.tsx
+++ drone-app/src/App.tsx
@@ -21,16 +21,18 @@ function updateSequencer(seq: Sequencer, state: AppState) {
 }
 
 
+function createSequencer(): Sequencer {
+  const audioEngine: AudioEngine = new AudioEngine();
+  const timer = new Timer();
+  return new Sequencer(timer, audioEngine);
+}
+
 export default function App() {
   const sequencerRef = useRef<Sequencer | null>(null);
-
   const [state, setState] = useState(loadState());
-  const [currentIndex, setCurrentIndex] = useState<number | null>(null);
 
   useEffect(() => {
-    const audioEngine: AudioEngine = new AudioEngine();
-    const timer = new Timer();
-    sequencerRef.current = new Sequencer(timer, audioEngine);
+    sequencerRef.current = createSequencer();
   }, []);
 
   useEffect(() => {
@@ -71,44 +73,10 @@ export default function App() {
     sequencerRef.current?.start();
   }
 
-  // function start() {
-  //   let i = 0;
-  //   const beat = 60 / state.bpm;
-
-  //   function playNext() {
-  //     if (i >= state.sequence.length) {
-  //       if (state.loop) {
-  //         i = 0;
-  //       } else {
-  //         setCurrentIndex(null);
-  //         return;
-  //       }
-  //     }
-
-  //     const note = state.sequence[i];
-  //     const freq = noteToFreq(note);
-  //     setCurrentIndex(i);
-  //     engine.playFreq(new AudioEngineNote(freq, beat));
-
-  //     i++;
-
-  //     setTimeout(playNext, beat * 1000);
-  //   }
-
-  //   playNext();
-  // }
-
-  // function stop() {
-  //   setCurrentIndex(null);
-  // }
-
-  // function reset() {
-  //   setCurrentIndex(null);
-  // }
 
   return (
     <div className="app">
-      <SequenceGrid sequence={state.sequence} currentIndex={currentIndex} />
+      <SequenceGrid sequence={state.sequence} currentIndex={sequencerRef.current?.sequenceIndex || 0} />
 
       <Controls
         bpm={state.bpm}
@@ -132,21 +100,4 @@ export default function App() {
     </div>
   );
 }
-
-// function noteToFreq(note: string): number {
-//   const A4 = 440;
-//   const map: Record<string, number> = {
-//     C: -9, "C#": -8, Db: -8,
-//     D: -7, "D#": -6, Eb: -6,
-//     E: -5,
-//     F: -4, "F#": -3, Gb: -3,
-//     G: -2, "G#": -1, Ab: -1,
-//     A: 0, "A#": 1, Bb: 1,
-//     B: 2
-//   };
-
-//   const name = note.slice(0, -1);
-//   const oct = Number(note.slice(-1));
-//   const semitone = map[name] + (oct - 4) * 12;
-//   return A4 * Math.pow(2, semitone / 12);
-// }
+ 
