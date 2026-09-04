ff79c4431f0b9f5c7b6e2614d64b73bca9df857f
ettore-galli <ettore.galli@gmail.com>
2026-09-04 20:19:38 +0200

    Nice controls

--- drone-app/src/components/Controls.tsx
+++ drone-app/src/components/Controls.tsx
@@ -25,28 +25,30 @@ export function Controls({
     <div className="controls">
 
       <div className="controls-row">
-        <button onClick={onStart}>START</button>
-        <button onClick={onStop}>STOP</button>
-        <button onClick={onReset}>RESET</button>
-      </div>
+        
+        {/*  */}
+        <button className="double-width" onClick={onStart}>START</button>
+        <button className="double-width" onClick={onStop}>STOP</button>
+        <button className="double-width" onClick={onReset}>RESET</button>
 
-      <div className="controls-row">
-        <span className="label-display">BPM</span><span className="value-display">{bpm}</span>
+        {/*  */}
+        <span className="label-display">BPM</span>
+        <span className="value-display">{bpm}</span>
         <button onClick={() => onBpmChange(-5)}>−</button>
         <button onClick={() => onBpmChange(+5)}>+</button>
-        <span className="label-display">BPM</span>
+        <span className="label-display">Loop</span>
         <button className={loop ? "loop-on" : "loop-off"} onClick={onToggleLoop}>
           {loop ? "ON" : "OFF"}
         </button>
-      </div>
 
-      <div className="controls-row">
+        {/*  */}
         <span className="label-display">8va</span>
         <span className="value-display">{octave}</span>
         <button onClick={() => onOctaveChange(octave - 1)}>-</button>
         <button onClick={() => onOctaveChange(octave + 1)}>+</button>
         <span className="label-display">&nbsp;</span>
         <span className="label-display">&nbsp;</span>
+
       </div>
 
     </div>
