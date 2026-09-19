3a5c28212c9dc92618e1621234e4c32cb8ccee38
ettore-galli <ettore.galli@gmail.com>
2026-09-19 19:25:20 +0200

    ui

--- drone-app/src/components/Controls.tsx
+++ drone-app/src/components/Controls.tsx
@@ -41,8 +41,8 @@ export function Controls({
 
         {/*  */}
         <span className="label-display mid-big-font">BPM</span>
-        <span className="value-display big-font">{bpm}</span>
         <button className="big-font" onClick={() => onBpmChange(-5)}>{MINUS_SIGN_SYMBOL}</button>
+        <span className="value-display big-font">{bpm}</span>
         <button className="big-font" onClick={() => onBpmChange(+5)}>{PLUS_SIGN_SYMBOL}</button>
         <span className="label-display mid-big-font">LOOP</span>
         <button className={loopButtonClass} onClick={onToggleLoop}>
@@ -51,8 +51,8 @@ export function Controls({
 
         {/*  */}
         <span className="label-display big-font">8<span className="label-display big-font superscript">va</span></span>
-        <span className="value-display big-font">{octave}</span>
         <button className="big-font" onClick={() => onOctaveChange(octave - 1)}>{MINUS_SIGN_SYMBOL}</button>
+        <span className="value-display big-font">{octave}</span>
         <button className="big-font" onClick={() => onOctaveChange(octave + 1)}>{PLUS_SIGN_SYMBOL}</button>
         <span className="label-display">&nbsp;</span>
         <span className="label-display">&nbsp;</span>
